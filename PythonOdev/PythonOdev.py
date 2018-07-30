e3cccc88c82d7b9e18e4be9703b983aa67e30916
ElifYonel <41699132+ElifYonel@users.noreply.github.com>
2018-07-30 22:07:36 +0300

    Add files via upload

---
+++ PythonOdev/PythonOdev.py
@@ -0,0 +1,15 @@
+# Cumartesi ve pazar günleri çalýþmýyoruz. Dolayýsýyla 22 gün çalýþýyoruz. Evden iþe gitmek için kullandýðýmýz vasýtanýn
+# ücreti 1.5 TL, iþten eve dönmek için kullandýðýmýz vasýtanýn ücreti 1.4 TL'dir. Aylýk ve yýllýk yol masrafýný bulan
+# programý yazýnýz.
+
+#Masraf= Gün sayýsý * (Gidiþ ve dönüþ ücretleri toplamý)
+
+gün_sayisi=22
+Gidis_ucreti=1.5
+Donus_ucreti=1.4
+
+Aylýk_Masraf= 22 * (1.5 + 1.4)
+print ("Aylýk masrafýnýz=",Aylýk_Masraf,"TL")
+
+Yýllýk_Masraf= gün_sayýsý * 12
+print ("Yýllýk masrafýnýz=",Yýllýk_Masraf,"TL")
